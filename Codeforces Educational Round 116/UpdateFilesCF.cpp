8b10fd471df7008e709b49f2c1bf3f582db2196c
shivanshjoshi7 <90916063+shivanshjoshi7@users.noreply.github.com>
2021-10-30 14:53:23 +0530

    Commit 1
    https://codeforces.com/contest/1606/problem/B

---
+++ Codeforces Educational Round 116/UpdateFilesCF.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long int n,k;
+		cin>>n>>k;
+
+		long long int i=1;
+		long long int sum=0;
+		long long int count=0;
+		while(i<=k && sum<n-1)
+		{
+
+			sum+=i;
+			count++;
+			i*=2;
+
+		}
+		if(sum>=n-1)
+		{
+			cout<<count<<endl;
+		}
+		else
+		{
+			if(((n-1)-sum)%k==0)
+			{
+				count=count+((n-1)-sum)/k;
+			}
+			else
+			{
+				count=count+1+((n-1)-sum)/k;
+			}
+			cout<<count<<endl;
+		}
+
+
+	}
+}
